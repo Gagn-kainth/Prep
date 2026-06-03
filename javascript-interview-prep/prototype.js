727a0bc35376459c223a7cc1fad1f65d58fb70e6
Gagn-kainth <preetgagan710@gmail.com>
2026-06-03 23:26:39 +0530

    5th prototype-chaining

--- javascript-interview-prep/prototype.js
+++ javascript-interview-prep/prototype.js
@@ -15,3 +15,18 @@ let obj2 ={
 Object.setPrototypeOf(obj, obj2);
 //obj.__proto__ = obj2; // another way to set the prototype of obj to obj2
 obj.greet(); // Output: hello my name is Alice and my age is 30, obj can access properties and methods of obj2 through the prototype chain
+
+//prototype chaining example
+let grandParent = {
+    grandParentProp: "I am the grandparent"
+};
+
+let parent = Object.create(grandParent);
+parent.parentProp = "I am the parent";
+
+let child = Object.create(parent);
+child.childProp = "I am the child";
+
+console.log(child.childProp); // Output: I am the child (found locally on child)
+console.log(child.parentProp); // Output: I am the parent (found on parent through prototype chain)
+console.log(child.grandParentProp); // Output: I am the grandparent (found on grandParent through prototype chain)
